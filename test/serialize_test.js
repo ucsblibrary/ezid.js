42638b7e0dba653ae83a56fda9d4d5102d594cb2
Alex Dunn <dunn.alex@gmail.com>
2018-03-14 11:09:12 -0700

    add parsing tests for dc and datacite

--- test/serialize_test.js
+++ test/serialize_test.js
@@ -2,7 +2,36 @@ const assert = require('assert')
 const fs = require('fs')
 const serialize = require('../lib/serialize.js')
 
-const hash = {
+const datacite = {
+  id: 'ark:/99999/fk4bg3w58x',
+  _updated: '1521050852',
+  _profile: 'datacite',
+  'datacite.title': 'god',
+  _export: 'yes',
+  'datacite.creator': 'nobody',
+  _owner: 'apitest',
+  _ownergroup: 'apitest',
+  _target: 'https://ezid.cdlib.org/id/ark:/99999/fk4bg3w58x',
+  _created: '1521050852',
+  'datacite.publicationyear': '0',
+  _status: 'public'
+}
+
+const dc = {
+  id: 'ark:/99999/fk4g74m02z',
+  _updated: '1521050568',
+  _target: 'https://ezid.cdlib.org/id/ark:/99999/fk4g74m02z',
+  _profile: 'dc',
+  _export: 'yes',
+  _owner: 'apitest',
+  'dc.creator': 'myself',
+  _ownergroup: 'apitest',
+  _created: '1521050568',
+  _status: 'public',
+  'dc.title': 'me'
+}
+
+const erc = {
   id: 'ark:/48907/f31g0m45',
   '_updated': '1477084593',
   '_target': 'http://alexandria.ucsb.edu/lib/ark:/48907/f31g0m45',
@@ -22,18 +51,36 @@ describe('serialize.in', () => {
   it('should correctly parse an ERC response', () => {
     const file = fs.readFileSync(`${__dirname}/fixtures/erc.txt`).toString()
 
-    assert.deepEqual(serialize.in(file), hash)
+    assert.deepEqual(serialize.in(file), erc)
   })
 
   it('should correctly serialize an ERC request body', () => {
     const file = fs.readFileSync(`${__dirname}/fixtures/erc.txt`).toString()
 
-    assert.deepEqual(serialize.out(hash), file.replace(/^success.*/, ''))
+    assert.deepEqual(serialize.out(erc), file.replace(/^success.*/, ''))
   })
 
   it('should correctly parse a DublinCore response', () => {
+    const file = fs.readFileSync(`${__dirname}/fixtures/dc.txt`).toString()
+
+    assert.deepEqual(serialize.in(file), dc)
+  })
+
+  it('should correctly serialize a DublinCore request body', () => {
+    const file = fs.readFileSync(`${__dirname}/fixtures/dc.txt`).toString()
+
+    assert.deepEqual(serialize.out(dc), file.replace(/^success.*/, ''))
   })
 
   it('should correctly parse a DataCite response', () => {
+    const file = fs.readFileSync(`${__dirname}/fixtures/datacite.txt`).toString()
+
+    assert.deepEqual(serialize.in(file), datacite)
+  })
+
+  it('should correctly serialize a DublinCore request body', () => {
+    const file = fs.readFileSync(`${__dirname}/fixtures/datacite.txt`).toString()
+
+    assert.deepEqual(serialize.out(datacite), file.replace(/^success.*/, ''))
   })
 })
