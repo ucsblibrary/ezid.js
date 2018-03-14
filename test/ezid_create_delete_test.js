173dc552f23acf9caaf1ee3eb30d7577c1ea3dd1
Alex Dunn <dunn.alex@gmail.com>
2018-03-14 15:52:08 -0700

    add test for #create/#delete

---
+++ test/ezid_create_delete_test.js
@@ -0,0 +1,27 @@
+const assert = require('assert')
+const EzID = require('../lib/ezid.js')
+
+const client = new EzID(
+  { username: 'apitest',
+    password: process.env.EZID_PASS,
+    shoulder: 'ark:/99999/fk4' }
+)
+
+describe('EzID#create and #delete', () => {
+  it('should successfully create an ARK with the specified ID, then delete it', () => {
+    const metadata = {
+      'dc.title': 'test thing',
+      id: 'ark:/99999/fk4ucsbt',
+      '_profile': 'dc',
+      '_status': 'reserved'
+    }
+
+    return client.create(metadata).then(response => {
+      assert.equal(response.id, 'ark:/99999/fk4ucsbt')
+
+      return client.delete(metadata.id).then(response => {
+        assert.equal(response.id, 'ark:/99999/fk4ucsbt')
+      })
+    })
+  })
+})
