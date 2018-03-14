b0853ed35a39ec59870e9704cd7442b6fb281bd1
Alex Dunn <dunn.alex@gmail.com>
2018-03-14 16:05:39 -0700

    add test for EzID#createOrUpdate

---
+++ test/ezid_create_or_update_test.js
@@ -0,0 +1,50 @@
+const assert = require('assert')
+const EzID = require('../lib/ezid.js')
+
+const client = new EzID(
+  { username: 'apitest',
+    password: process.env.EZID_PASS,
+    shoulder: 'ark:/99999/fk4' }
+)
+
+const original = {
+  'dc.title': 'test thing',
+  id: 'ark:/99999/fk4ucsbt0',
+  '_profile': 'dc'
+}
+
+const updated = {
+  'dc.title': 'real thing',
+  'dc.publisher': 'real deal',
+  id: 'ark:/99999/fk4ucsbt0',
+  '_profile': 'dc'
+}
+
+describe('EzID#createOrUpdate', () => {
+  it('should successfully create an ARK, then update it', () => {
+    return client.createOrUpdate(original).then(response => {
+      assert.equal(response.id, original.id)
+
+      return client.createOrUpdate(updated).then(response => {
+        assert.equal(response.id, updated.id)
+
+        return EzID.get(updated.id).then(info => {
+          assert.deepEqual(
+            info,
+            Object.assign(
+              updated, {
+                "_created": "1521068520",
+                "_export": "yes",
+                "_owner": "apitest",
+                "_ownergroup": "apitest",
+                "_status": "public",
+                "_target": "https://ezid.cdlib.org/id/ark:/99999/fk4ucsbt0",
+                "_updated": "1521068618"
+              }
+            )
+          )
+        })
+      })
+    })
+  })
+})
